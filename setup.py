b3e5b59547110c7598cd9a9918d004c779cbe563
Sonic4999 <sonictbm@gmail.com>
2021-12-19 14:38:40 -0500

    Appended b to version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Astrea49",
     url="https://github.com/Astrea49/molter",
-    version="0.0.1",
+    version="0.0.1b",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=requirements,
