90c75fdd42fd81b88a69a6c189201a8ecca668c0
Sonic4999 <sonictbm@gmail.com>
2021-12-19 01:32:42 -0500

    LICENSE adjustments, __init__ file

---
+++ molter/__init__.py
@@ -0,0 +1,13 @@
+"""
+Molter
+~~~~~~~~~~~~~~~~~~~
+Shedding a new skin on Dis-Snek's commands.
+:copyright: (c) 2021-present Astrea49
+:license: MIT, see LICENSE for more details.
+"""
+
+__version__ = "0.0.1"
+
+from .command import *
+from .converters import *
+from .errors import *
