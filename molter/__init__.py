4ec9af0716bd6fc5b75b58ff9b5b05ef780c4051
Sonic4999 <sonictbm@gmail.com>
2021-12-21 20:18:29 -0500

    Version bump

--- molter/__init__.py
+++ molter/__init__.py
@@ -6,7 +6,7 @@ Shedding a new skin on Dis-Snek's commands.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 from .command import *
 from .converters import *
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Astrea49",
     url="https://github.com/Astrea49/molter",
-    version="0.3.0",
+    version="0.3.1",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=requirements,
