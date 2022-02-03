462ef525ab02cf7e4f2d62e156e04634a3ee0d32
Sonic4999 <sonictbm@gmail.com>
2022-02-02 22:42:09 -0500

    chore: version bump!

--- molter/__init__.py
+++ molter/__init__.py
@@ -6,7 +6,7 @@ Shedding a new skin on Dis-Snek's commands.
 :license: MIT, see LICENSE for more details.
 """
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 
 from .command import *
 from .converters import *
